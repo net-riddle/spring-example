a46674cc75a9eab745261de6048b3b2faa88e34b
danb <accessoitaliano@gmail.com>
2025-05-16 11:29:49 +0200

    main controller

---
+++ src/main/java/com/netriddle/spring_example/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.netriddle.spring_example.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public ResponseEntity<Object> rootPath(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
